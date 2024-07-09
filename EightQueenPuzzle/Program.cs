145db9ba406313fe544d3e2bb068795d028fd3f3
StefanBoudri <104310664+StefanBoudri@users.noreply.github.com>
2024-07-09 14:53:43 +0200

    Add project files.

---
+++ EightQueenPuzzle/Program.cs
@@ -0,0 +1,9 @@
+﻿using NQueenPuzzle;
+
+Console.WriteLine("This is the n queens puzzle. For n is the chessboard n*n and n is the amount of queens.");
+Console.WriteLine("what do you want n to be?");
+string input = Console.ReadLine();
+
+int n = int.Parse(input);
+
+Puzzle _ = new(n);
---
+++ EightQueenPuzzle/Puzzle.cs
@@ -0,0 +1,95 @@
+﻿namespace NQueenPuzzle;
+
+public class Puzzle
+{
+    public Puzzle(int n)
+    {
+        int[,] chessBoard = new int[n, n];
+        SolutionFinder(n, chessBoard);
+    }
+
+    private static void SolutionFinder(int n, int[,] chessBoard)
+    {
+        int totalPlacedQueens = 0;
+        for (int row = 0; row < n; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (IsSafe(chessBoard, row, col, n))
+                {
+                    chessBoard[row, col] = 1;
+                    totalPlacedQueens++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(chessBoard[i, j] + " ");
+            }
+                Console.WriteLine();
+        }
+
+    }
+
+    private static bool IsSafe(int[,] chessBoard, int row, int col, int n)
+    {
+        // Check for queen in same row
+        for(int y = col; y >= 0; y--)
+        {
+            if (chessBoard[row, y] == 1)
+            {
+                return false;
+            }
+        }
+
+        // Check for queen above
+        for (int x = row; x >= 0; x--)
+        {
+            if (chessBoard[x, col] == 1)
+            {
+                return false;
+            }
+        }
+
+        //check for queen diagonally (upper left)
+        for (int x = row, y = col; y >= 0 && x >= 0; y--, x--)
+        {
+            if (chessBoard[x, y] == 1)
+            {
+                return false;
+            }
+        }
+
+        //check for queen diagonally (lower left)
+        for (int x = row, y = col; x < n && y >= 0; x++, y--)
+        {
+            if (chessBoard[x, y] == 1)
+            {
+                return false;
+            }
+        }
+
+        // Check for queen diagonally (upper right)
+        for (int x = row, y = col; x >= 0 && y < n; x--, y++)
+        {
+            if (chessBoard[x, y] == 1)
+            {
+                return false;
+            }
+        }
+
+        // Check for queen diagonally (lower right)
+        for (int x = row, y = col; x >= 0 && y < n; x--, y++)
+        {
+            if (chessBoard[x, y] == 1)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
